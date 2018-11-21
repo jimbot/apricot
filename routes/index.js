fa0e940c89d507b795babe20b007f4998f9ef9f5
James Chen <jameschen1295@gmail.com>
2018-11-21 07:13:57 -0800

    project owners now see a slightly differently card ui and have the option to update their post

--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,36 @@ router.get("/register", function(req, res){
 
 // handling user sign up
 router.post("/register", usernameToLowerCase, function(req, res){
-  var pics = [
-    "http://gdurl.com/XG8G",
-    "http://gdurl.com/nrjZ",
-    "http://gdurl.com/M5Fd"
+  var temp = [
+    "https://i.imgur.com/s11FC6a.jpg",
+    "https://i.imgur.com/yRL2B75.jpg",
+    "https://i.imgur.com/a8x9yNn.jpg",
+    "https://i.imgur.com/fbI8YX3.jpg",
+    "https://i.imgur.com/doWmlMF.jpg",
+    "https://i.imgur.com/jqQbMwv.jpg",
+    "https://i.imgur.com/9SV8l8J.jpg",
+    "https://i.imgur.com/E3IVuYR.jpg",
+    "https://i.imgur.com/p5LMaNO.jpg",
+    "https://i.imgur.com/LSxrKkC.jpg",
+    "https://i.imgur.com/ZsU18RH.jpg",
+    "https://i.imgur.com/jz62sV0.jpg",
+    "https://i.imgur.com/y0sVg20.jpg",
+    "https://i.imgur.com/3vdlodF.jpg",
+    "https://i.imgur.com/8Y9w8K8.jpg",
+    "https://i.imgur.com/diyedJ9.jpg",
+    "https://i.imgur.com/PZRZJ91.jpg",
+    "https://i.imgur.com/0IvBke6.jpg",
+    "https://i.imgur.com/IpKxyzP.jpg",
+    "https://i.imgur.com/P2un1WU.jpg",
+    "https://i.imgur.com/G1zStbw.jpg",
+    "https://i.imgur.com/sRUc4h2.jpg"
   ];
-  var num = Math.floor((Math.random() * 2));
+  var num = Math.floor((Math.random() * (temp.length - 1)));
   var newUser = new User({
     username: req.body.username,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    profilePicture: pics[num]
+    profilePicture: temp[num]
   });
   User.register(newUser, req.body.password, function(err, user){
     if(err){
